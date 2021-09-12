5b517588e5f8edad05b3a4acf749b1f29b90c494
dscyrescotti <dscyrescotti@gmail.com>
2021-09-12 13:05:59 +0630

    feat: commit initially

--- Package.swift
+++ Package.swift
@@ -5,24 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "ReachabilityX",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ReachabilityX",
             targets: ["ReachabilityX"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ReachabilityX",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Reachability", package: "Reachability")
+            ]
+        ),
         .testTarget(
             name: "ReachabilityXTests",
-            dependencies: ["ReachabilityX"]),
+            dependencies: ["ReachabilityX"]
+        ),
     ]
 )
