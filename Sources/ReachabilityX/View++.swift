c7e586e542cf7e6b590932c04df77e10ac0949e7
dscyrescotti <dscyrescotti@gmail.com>
2021-09-26 13:22:56 +0630

    feat: implement view modifiers

--- Sources/ReachabilityX/View++.swift
+++ Sources/ReachabilityX/View++.swift
@@ -13,4 +13,22 @@ public extension View {
     func onChangeStatus(action: @escaping (Status) -> Void) -> some View {
         self.environment(\.changeStatusAction, action)
     }
+    
+    func onChangeInterfaceType(_ reachability: Reachability, action: @escaping (InterfaceType) -> Void) -> some View {
+        self.onReceive(reachability.objectWillChange, perform: {
+            action(reachability.interfaceType)
+        })
+    }
+    
+    func onChangePath(_ reachability: Reachability, action: @escaping (NWPath) -> Void) -> some View {
+        self.onReceive(reachability.objectWillChange, perform: {
+            action(reachability.path)
+        })
+    }
+    
+    func onChangeStatus(_ reachability: Reachability, action: @escaping (Status) -> Void) -> some View {
+        self.onReceive(reachability.objectWillChange, perform: {
+            action(reachability.status)
+        })
+    }
 }
